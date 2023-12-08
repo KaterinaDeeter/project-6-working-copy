ea5364cf1e94ff7aecd1e6dea5deae8a7183e52c
Katerina Deeter <ktdeeter@gmail.com>
2023-12-07 20:58:03 -0700

    updated

--- aggregator/src/main/java/com/example/aggregator/service/AggregatorService.java
+++ aggregator/src/main/java/com/example/aggregator/service/AggregatorService.java
@@ -4,6 +4,9 @@ import com.example.aggregator.client.AggregatorRestClient;
 import com.example.aggregator.model.Entry;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class AggregatorService {
 
@@ -15,5 +18,29 @@ public class AggregatorService {
 
     public Entry getDefinitionFor(String word) {
         return restClient.getDefinitionFor(word);
+    }
+
+    public List<Entry> getWordsThatContainSuccessiveLettersAndStartsWith(String chars) {
+
+        List<Entry> wordsThatStartWith = restClient.getWordsStartingWith(chars);
+        List<Entry> wordsThatContainSuccessiveLetters = restClient.getWordsThatContainConsecutiveLetters();
+
+        List<Entry> common = new ArrayList<>(wordsThatStartWith);
+        common.retainAll(wordsThatContainSuccessiveLetters);
+
+        return common;
+    }
+
+    public List<Entry> getWordsThatContainSuccessiveLettersAndContains(String chars) {
+
+        List<Entry> wordsThatContain = restClient.getWordsThatContain(chars);
+        List<Entry> wordsThatContainSuccessiveLetters = restClient.getWordsThatContainConsecutiveLetters();
+
+        List<Entry> common = new ArrayList<>(wordsThatContain);
+        common.retainAll(wordsThatContainSuccessiveLetters);
+
+        return common;
+
+
     }
 }
